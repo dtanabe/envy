75becc234365ed0b1d163e155c5b31a793523d5a
Davin K. Tanabe <gitter@pelebyte.net>
2021-01-11 13:06:58 -0500

    Light vscode settings changes; also load sdkman if present.

---
+++ etc/bash/profile.d/60-sdkman.sh
@@ -0,0 +1,5 @@
+# shellcheck shell=bash
+# shellcheck disable=SC1090
+# SC1090: Ignored because we're intentionally sourcing arbitrary paths
+
+[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
