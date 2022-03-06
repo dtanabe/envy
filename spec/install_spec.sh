a6b4c8991a2caaa3bdfaa281e3842915887133ca
Davin K. Tanabe <gitter@pelebyte.net>
2022-03-05 20:29:45 -0500

    output tweaks

--- spec/install_spec.sh
+++ spec/install_spec.sh
@@ -7,13 +7,16 @@ Describe '.installer/install.sh'
     BeforeRun 'export PATH=/bin'
 
     When run script .installer/install.sh
+    The output should eq "Checking prerequisites..."
     The status should be failure
     The error should eq "git could not be found"
   End
 
   It 'does not run unless HOME is defined'
     BeforeRun 'export HOME='
+
     When run script .installer/install.sh
+    The output should eq "Checking prerequisites..."
     The status should be failure
     The error should eq "Could not determine \$HOME"
   End
