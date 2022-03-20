20cac5253d7aa573baf17a4ad53b59d7380b20b9
Davin K. Tanabe <gitter@pelebyte.net>
2022-03-19 20:59:19 -0400

    browser rule tweaks

--- etc/hammerspoon/browser.lua
+++ etc/hammerspoon/browser.lua
@@ -1,13 +1,17 @@
 local chrome = require("chrome")
 
 local da = {
-  'circleci.com',
-  'console.cloud.google.com',
-  'docs.google.com',
+  'digitalasset1.lightning.force.com',
+  'digitalasset.zoom.us',
   'discuss.daml.com',
   'figma.com',
   'github.com',
   'lucid.app',
+}
+local subdomains = {
+  'circleci.com',
+  'datadoghq.com',
+  'google.com',
 }
 
 local function safari(url)
@@ -18,11 +22,18 @@ end
 hs.urlevent.setDefaultHandler('http')
 hs.urlevent.setDefaultHandler('https')
 hs.urlevent.httpCallback = function(scheme, host, _, fullURL, senderPID)
+  for _, k in ipairs(subdomains) do
+    if host:sub(-#k) == k then
+      chrome.open("DA", fullURL)
+    end
+  end
+
   for _, k in ipairs(da) do
     if k == host then
       chrome.open("DA", fullURL)
       return
     end
   end
+
   safari(fullURL)
 end
