5f984a570ccc53ff71498e4e33e116c1704504ad
Davin K. Tanabe <gitter@pelebyte.net>
2026-06-16 23:41:54 -0400

    nix config

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "envy-profile";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.buildEnv {
+          name = "envy-profile";
+          paths = with pkgs; [
+            gh
+            git
+            jq
+            nixfmt
+            starship
+            yq
+          ];
+        };
+      }
+    );
+}
